7e814576a71bc2e0d808de0c77079537892a8a15
dominikjaro <dominikjaro789@gmail.com>
2024-07-21 13:46:03 +0100

    Pushing dynamic blocks and expressions

---
+++ dynamic blocks/main.tf
@@ -0,0 +1,48 @@
+ terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.38.0"
+    }
+  }
+}
+
+provider "google" {
+    project = "my-project-id"
+  region = "europe-west2"
+}
+
+
+data "google_compute_zones" "available" {
+  region = "europe-west2"
+}
+
+resource "google_compute_security_policy" "policy" {
+  name = "my-policy"
+
+  rule {
+    action   = "deny(403)"
+    priority = "1000"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["9.9.9.0/24"]
+      }
+    }
+    description = "Deny access to IPs in 9.9.9.0/24"
+  }
+
+  rule {
+    action   = "allow"
+    priority = "2147483647"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+    description = "default rule"
+  }
+}
+
+
---
+++ expressions/main.tf
@@ -0,0 +1,16 @@
+terraform {
+
+}
+
+variable "worlds" {
+    type = list(string)
+}
+
+variable "worlds_map" {
+    type = map
+  
+}
+
+variable "worlds_splat" {
+    type = list
+}
